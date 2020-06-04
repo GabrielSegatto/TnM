5b2679bf050fa7f7634a5f275bd3c3344451dd80
Gabriel Segatto <gabriel.segatto58@gmail.com>
2020-06-04 18:00:44 -0300

    fix modal on requisitos

--- 6.Requisito/api.js
+++ 6.Requisito/api.js
@@ -90,7 +90,8 @@ window.addEventListener('click', outsideClick);
 
 // Open
 function openModal() {
-  modal.style.display = 'block';
+    modal.style.display = 'flex';
+    modal.style.alignItems = 'center';
 }
 
 // Close
